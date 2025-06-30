c3c69659fb3d052ad900f2285a9662621e338915
M V Hemanth <venkatahemanth42d@gmail.com>
2025-06-30 11:50:32 +0530

    Updated Ports

--- Backend/index.js
+++ Backend/index.js
@@ -6,10 +6,11 @@ const db = require("./config/mongooseConnection");
 const cors = require("cors");
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URI,
     credentials: true,
   })
 );
@@ -21,6 +22,6 @@ app.use(cookieParser());
 
 app.use("/", userRouters);
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log(`Port is running at https://localhost:${8000}`);
 });
